79e0d0f5600e0ad777c2950044e3f4d567e434e6
PhilipCRivera <82003694+PhilipCRivera@users.noreply.github.com>
2021-04-27 15:09:06 -0700

    added code to main.js and created a index.html file

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+var str ="Don't you just love the color orange?" ;
+var result = str.fontcolor("orange");
+document.getElementById("Orange_Text").innerHTML= result;
+}
