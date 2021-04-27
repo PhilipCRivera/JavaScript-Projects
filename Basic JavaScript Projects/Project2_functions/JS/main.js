d1e58e500b53fd6e13f9647decc914388acc6307
PhilipCRivera <82003694+PhilipCRivera@users.noreply.github.com>
2021-04-27 16:05:31 -0700

    updates

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,13 @@ var str ="Don't you just love the color orange?" ;
 var result = str.fontcolor("orange");
 document.getElementById("Orange_Text").innerHTML= result;
 }
+
+function myFunction() {
+  var x = document.getElementById("Felipe").id;
+  document.getElementById("demo").innerHTML = x;
+}
+ function myFunction2() {
+     var sentence = "Long, long ago the Four Kingdoms lived in harmony,";
+     sentence += "until the Fire Nation came!";
+     document.getElementById("Concatenate").innerHTML = sentence;
+ }
