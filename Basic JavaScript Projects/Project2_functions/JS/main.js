f81bedaaf74ccb88255d738bda6749fec418b610
PhilipCRivera <82003694+PhilipCRivera@users.noreply.github.com>
2021-04-27 16:36:30 -0700

    index.html and main.js updates

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,15 +1,20 @@
 function My_First_Function() {
-var str ="Don't you just love the color orange?" ;
-var result = str.fontcolor("orange");
-document.getElementById("Orange_Text").innerHTML= result;
+var str ="Don't you just love the color orange?" ; // the variable str will display this text
+var result = str.fontcolor("orange");// the font color will be orange
+document.getElementById("Orange_Text").innerHTML= result;// the result will dispaly Don't you just love the color orange in orange font
 }
 
 function myFunction() {
-  var x = document.getElementById("Felipe").id;
-  document.getElementById("demo").innerHTML = x;
+  var x = document.getElementById("Felipe").id;// this will display Felipe as the id
+  document.getElementById("demo").innerHTML = x;// the value of demo will equal what x is 
 }
  function myFunction2() {
-     var sentence = "Long, long ago the Four Kingdoms lived in harmony,";
-     sentence += "until the Fire Nation came!";
-     document.getElementById("Concatenate").innerHTML = sentence;
+     var sentence = "Long, long ago the Four Kingdoms lived in harmony,";// this variable is a string
+     sentence += "until the Fire Nation came!";// this uses the operator += to continue the string
+     document.getElementById("Concatenate").innerHTML = sentence;// this states that the element id Concatenate will display the sentence variable
  }
+
+ function myFunction3() {
+var Date = document.getElementById().id;
+document.getElementById("demo").innerHTML = Date;
+}
