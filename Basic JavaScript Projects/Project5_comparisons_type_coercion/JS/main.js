e224c9317fba57410580b8006b8ff9aca500b26b
PhilipCRivera <82003694+PhilipCRivera@users.noreply.github.com>
2021-04-30 14:06:27 -0700

    created a new file

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,39 @@
+document.write("10" + 5); //this type coercion will take 5 and add it to the string 10 displaying 105
+
+document.write("Bulbasaur, I choose You!"); // this type of Operator will display the string
+
+document.write(72 + 28); // this type of Operator will display the output number- 
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = isNaN('this is a string!');
+}
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(200>199);
+
+document.write(200<199);
+
+console.log(235<65);
+
+document.write(25==25);
+
+document.write(25==45);
+
+x = 55
+y = 55
+document.write(x===y);
+
+x = 15
+y = "fifteen"
+document.write(x===y);
+
+x = 12
+y = "red"
+document.write(x===y);
+
+x = 27
+y = 72
+document.write(x===y);
